3ea168c85eef3f2fb6205c063372fbe88fb6f641
Adél Bajcsi <33290209+bajcsiadel@users.noreply.github.com>
2024-06-20 12:33:54 +0200

    5 add data analysis scripts (#8)
    Closes #5 
    
    * added hydra config dataclasses, logger
    * added script to select patients

---
+++ src/utils/config/__init__.py
@@ -0,0 +1,3 @@
+from hydra.core.config_store import ConfigStore
+
+config_store = ConfigStore.instance()
---
+++ src/utils/config/types.py
@@ -0,0 +1,2 @@
+from utils.config.data import DataConfig
+from utils.config.output import OutputConfig
